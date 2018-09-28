f0d84dc2047fabd62399aa3ef0f7c515065b2940
Chaow Porkaew <chaow.po@up.ac.th>
2018-09-28 13:16:26 +0700

    update skeleton view

--- src/router.js
+++ src/router.js
@@ -16,12 +16,12 @@ export default new Router({
       component: loadView('Home')
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/main',
+      name: 'main',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: loadView('About')
+      component: loadView('Main')
     }
   ]
 })
